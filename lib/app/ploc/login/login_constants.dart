22f12643a166f41e09a705571e3326b7883038c7
Nagarjun Nagesh <nagarjun_nagesh@outlook.com>
2021-07-20 17:44:10 +0200

    format document

--- lib/app/ploc/login/login_constants.dart
+++ lib/app/ploc/login/login_constants.dart
@@ -4,10 +4,6 @@ const String INVALID_INPUT_LOGIN_FAILURE_MESSAGE =
 const String GENERIC_LOGIN_FAILURE_MESSAGE =
     'An error occured while logging in. Please try again later!';
 const String EMAIL_INVALID = 'Email entered is invalid.';
-<<<<<<< Updated upstream
 const String PASSWORD_EMPTY = 'Password entered is empty.';
 const String PASSWORD_INVALID =
-=======
-const String PASSWORD_EMPTY = 'Password entered is empty.';const String PASSWORD_INVALID =
->>>>>>> Stashed changes
     'Password should contain atleast an uppercase, lowercase, numbers and special characters.';
